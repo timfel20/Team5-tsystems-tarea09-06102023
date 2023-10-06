f372fcfbbddc67307a2a92734a4d4181c72a7e00
Daniel Nuñez Romero <daninuro96@gmail.com>
2023-10-06 18:38:27 +0200

    Se añaden gett y sett a clase Raices

--- Ejercicio4Team5/src/raices/Raices.java
+++ Ejercicio4Team5/src/raices/Raices.java
@@ -8,10 +8,84 @@ public class Raices
 	private double b;
 	private double c;
 	
-	public Raices(double a, double b, double c) {
+	
+	
+
+
+
+	public Raices(double a, double b, double c) 
+	{
 		super();
 		this.a = a;
 		this.b = b;
 		this.c = c;
+		
+		
 	}
+
+
+
+
+
+
+
+	public double getA() {
+		return a;
+	}
+
+
+
+
+
+
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+
+
+
+
+
+
+	public double getB() {
+		return b;
+	}
+
+
+
+
+
+
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+
+
+
+
+
+
+	public double getC() {
+		return c;
+	}
+
+
+
+
+
+
+
+	public void setC(double c) {
+		this.c = c;
+	}
+	
+	
+	
+	
+	
+	
 }
