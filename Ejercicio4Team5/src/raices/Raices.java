3bc15a7f0d334ba1f54333d7e3fde0e411a9cdf7
ivandavid858 <ivandavid858@gmail.com>
2023-10-06 18:30:27 +0200

    Constructor añadido

--- Ejercicio4Team5/src/raices/Raices.java
+++ Ejercicio4Team5/src/raices/Raices.java
@@ -7,4 +7,11 @@ public class Raices
 	private double a;
 	private double b;
 	private double c;
+	
+	public Raices(double a, double b, double c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 }
